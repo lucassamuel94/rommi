2650ca07b174c5b8ccdf5740313fa35e526e00d2
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-03-04 18:41:26 -0300

    update: include request DatoCMS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
+import { HomePageDocument, HomePageQuery, SlideRecord } from '@/gql/generated'
+import { datoClient } from '@/lib/client'
+import { registerUrql } from '@urql/next/rsc'
 import Link from 'next/link'
+import { OperationResult } from 'urql'
 
-export default function HomeApp() {
+const { getClient } = registerUrql(datoClient)
+
+export default async function HomeApp() {
+  const result: OperationResult & HomePageQuery = await getClient().query(
+    HomePageDocument,
+    {}
+  )
+
+  const {
+    data: { homepage }
+  } = result
+
   return (
     <main className='container py-10'>
       <Link href={'/'}>
         <h1 className='font-medium'>Boilerplate Next14</h1>
       </Link>
+
+      <ul>
+        {homepage?.slides.map((slide: SlideRecord) => (
+          <li key={slide.nome}>{slide.nome}</li>
+        ))}
+      </ul>
     </main>
   )
 }
