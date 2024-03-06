7c67f49b9f5d16b0b068c1f1f2fb389fd7d0cdc2
Lucas Samuel <lucassamuel.dev@gmail.com>
2024-03-06 08:51:14 -0300

    styled: update component Button with variants and size

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,16 +1,16 @@
-import * as React from 'react'
 import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
+import * as React from 'react'
 
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
         default: 'bg-neutral-900 text-neutral-50 hover:bg-neutral-900/90',
-        destructive: 'bg-red-500 text-neutral-50 hover:bg-red-500/90',
+        primary: 'bg-main-500 text-neutral-800 hover:bg-main-400',
         outline:
           'border border-neutral-200 bg-white hover:bg-neutral-100 hover:text-neutral-900',
         secondary: 'bg-neutral-100 text-neutral-900 hover:bg-neutral-100/80',
@@ -18,7 +18,7 @@ const buttonVariants = cva(
         link: 'text-neutral-900 underline-offset-4 hover:underline'
       },
       size: {
-        default: 'h-10 px-4 py-2',
+        default: 'h-10 px-6 py-3',
         sm: 'h-9 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
         icon: 'h-10 w-10'
